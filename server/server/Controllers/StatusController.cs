82a3dc99ee9ce64e38a32f0fb9fe31f34657f990
cioarecalexandru <cioarec.alexandru96@yahoo.com>
2018-05-17 22:49:50 +0300

    Server project init

---
+++ server/server/Controllers/StatusController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using server.Models;
+
+namespace server.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Status")]
+    public class StatusController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            var test = new Test();
+            test.Message = "Hello World!";
+            return JsonConvert.SerializeObject(test);
+        }
+    }
+}
